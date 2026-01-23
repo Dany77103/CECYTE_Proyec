6f02460396f05db2417c037654d51b927f12ac1b
Dany77103 <dafloresza@gmail.com>
2026-01-22 18:46:49 -0600

    imagen de pestaña

--- asignar_qr.php
+++ asignar_qr.php
@@ -14,6 +14,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <meta charset="UTF-8">
     <title>Asignar Código QR - CECYTE</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
